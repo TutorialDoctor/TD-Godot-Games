d507b1fe3e651b4203bf4605f7d86e2e83b18d1a
TutorialDoctor <thetutorialdoctor@gmail.com>
2015-10-17 23:40:24 -0400

    final edit!

--- Boxy/Assets/scripts/Level1.gd
+++ Boxy/Assets/scripts/Level1.gd
@@ -10,19 +10,16 @@ var samples
 
 func _ready():
 	set_process(true)
-	
 	boxy_animation = get_node("AnimationPlayer")
 	boxy = get_node("Boxy")
 	boxy.set_contact_monitor(true)
 	boxy.set_max_contacts_reported(true)
 	boxy.set_mode(2)
 	boxy_girl = get_node("Boxy Girl")
-	
+	boxy_girl.set_mode(2)
 	ray = get_node("Boxy/RayCast2D")
 	ray.add_exception(boxy)	
-	
 	samples = get_node("SamplePlayer")
-	
 	restart_line = get_node("Restart Line")
 
 func _process(delta):
@@ -44,6 +41,13 @@ func _process(delta):
 		#get_tree().change_scene('res://Levels/Menu.xml')
 	
 	if restart_line in boxy.get_colliding_bodies():
+		get_tree().reload_current_scene()
+	
+	if Input.is_key_pressed(16777217): 
+	#16777217 is the scancode for the escape key under @GlobalScope in the API
+		get_tree().quit()
+	if Input.is_key_pressed(82):
+	#82 is the scancode for the 'R' key under @GlobalScope in the API
 		get_tree().reload_current_scene()
 
 func on_ground():
