225acb11708a55ffb3a74f14d2a57052819e3a4b
TutorialDoctor <thetutorialdoctor@gmail.com>
2016-04-17 18:59:01 -0400

    AAA Game menu
    The start of a AAA game menu.

---
+++ AAA Game Menu/Assets/scripts/navigate.gd
@@ -0,0 +1,24 @@
+
+extends VButtonArray
+
+# Google "creative commons music" for free music.
+# http://freemusicarchive.org/genre/Instrumental/
+# Ars Sonor "Jasmina's Song (Mona Lisa Overdrive Mix for Jasmina Olsson)""In Search of Balance (Among the Shadows)" 
+#Drone, Industrial, Ambient
+# grab_focus() is important
+
+var samples
+
+func _ready():
+	set_process_input(true)
+	grab_focus() # put focus on the button array
+	samples = get_parent().get_node("SamplePlayer")
+	
+
+func _input(event):
+	print(event.is_action_pressed("ui_accept"))
+	if event.is_action_pressed("ui_down"):
+		samples.play("Blip_Select3")
+	elif event.is_action_pressed("ui_up"):
+		samples.play("Blip_Select3")
+
---
+++ AAA Game Menu/readme.md
@@ -0,0 +1,1 @@
+AAA game menu in Godot.
